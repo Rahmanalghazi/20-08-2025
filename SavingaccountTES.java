fa06694fb34b9e1a998c664872c872402ae4ad59
Rahmanalghazi <abdurrahman_ghazi_34rpl@student.smktelkom-mlg.sch.id>
2025-08-20 08:53:15 +0700

    Add files via upload

---
+++ SavingaccountTES.java
@@ -0,0 +1,29 @@
+public class SavingaccountTES {
+    public static void main(String[] args) {
+           
+SavingsAccount sa0001 = new SavingsAccount();
+sa0001.balance = 7000;
+sa0001.name = "Fardan";
+sa0001.interestRate = 0.07;
+sa0001.displayCustomer();
+System.out.println(""); 
+
+SavingsAccount sa0002 = new SavingsAccount();
+sa0002.balance = 2000;
+sa0002.name = "Bayu";
+sa0002.interestRate = 0.05;
+sa0002.displayCustomer();
+System.out.println(""); 
+
+SavingsAccount sa0003= new SavingsAccount();
+sa0003.balance = 3000;
+sa0003.name = "Gita";
+sa0003.interestRate = 0.03;
+sa0003.displayCustomer();
+System.out.println(""); 
+
+    }
+
+    
+}
+
